80d3476b70d18ac1a16aa5b0744b8f3bf0c6eff7
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2017-04-05 15:33:17 +0300

    Improvements and bugfixes to dynarr

--- dynarr/dynarr.h
+++ dynarr/dynarr.h
@@ -20,6 +20,31 @@
     .size = 0, \
   }
 
+#define DYNARR_INIT(arptr) \
+  do { \
+    typeof(arptr) __init_ar = (arptr); \
+    __init_ar->ar = NULL; \
+    __init_ar->capacity = 0; \
+    __init_ar->size = 0; \
+  } \
+  while(0)
+
+#define DYNARR_FREE(arptr) \
+  do { \
+    typeof(arptr) __free_ar = (arptr); \
+    free(__free_ar->ar); \
+    __free_ar->ar = NULL; \
+    __free_ar->capacity = 0; \
+    __free_ar->size = 0; \
+  } \
+  while(0)
+
+#define DYNARR_CLEAR(arptr) \
+  do { \
+    typeof(arptr) __clear_arptr = (arptr); \
+    __clear_arptr->size = 0; \
+  } while(0)
+
 #define DYNARR_GET(arptr, i) \
   ((arptr)->ar[i])
 
@@ -72,14 +97,13 @@ void *dynarr_realloc_impl(void *old, size_t size, size_t *capacity, size_t exten
     __push_back_result; \
   })
 
-#define DYNARR_PUSH_BACK_RETPTR(arptr, x) \
+#define DYNARR_PUSH_BACK_RETPTR(arptr) \
   ({ \
     typeof(arptr) __push_back_retptr_arptr = (arptr); \
-    typeof(x) __push_back_retptr_x = (x); \
-    typeof(arptr->ar) __push_back_retptr_result = NULL; \
+    typeof((arptr)->ar) __push_back_retptr_result = NULL; \
     if (DYNARR_ENSURE_EXTEND(__push_back_retptr_arptr, 1)) \
     { \
-      __push_back_retptr_result = &__push_back_retptr_arptr->[__push_back_retptr_arptr->size++]; \
+      __push_back_retptr_result = &__push_back_retptr_arptr->ar[__push_back_retptr_arptr->size++]; \
     } \
     else \
     { \
