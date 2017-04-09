e87de7243b8e024bc9dbbd7896cd7fc4e5c7a2d0
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2017-04-09 16:35:20 +0300

    Add iphdr change forgotten from last commit

--- iphdr/iphdr.h
+++ iphdr/iphdr.h
@@ -317,6 +317,12 @@ static inline int tcp_ack(const void *pkt)
   return !!(hdr_get8h(&cpkt[13]) & (1<<4));
 }
 
+static inline void tcp_set_ack_on(void *pkt)
+{
+  char *cpkt = pkt;
+  hdr_set8h(&cpkt[13], hdr_get8h(&cpkt[13]) | (1<<4));
+}
+
 static inline int tcp_rst(const void *pkt)
 {
   const char *cpkt = pkt;
