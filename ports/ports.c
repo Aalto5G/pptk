dcb1278e1cb706a10903f142e7eb073b78ca9f58
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2017-04-13 22:39:58 +0300

    Add missing linked list ports

--- ports/ports.c
+++ ports/ports.c
@@ -1,1 +1,7 @@
 #include "ports.h"
+
+void linkedlistfunc(struct packet *pkt, void *userdata)
+{
+  struct linkedlistfunc_userdata *ud = userdata;
+  linked_list_add_tail(&pkt->node, ud->head);
+}
--- ports/ports.h
+++ ports/ports.h
@@ -2,10 +2,17 @@
 #define _PORTS_H_
 
 #include "packet.h"
+#include "linkedlist.h"
 
 struct port {
   void (*portfunc)(struct packet *pkt, void *userdata);
   void *userdata;
 };
+
+struct linkedlistfunc_userdata {
+  struct linked_list_head *head;
+};
+
+void linkedlistfunc(struct packet *pkt, void *userdata);
 
 #endif
