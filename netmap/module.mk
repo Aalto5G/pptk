eb25b95a5e63d094dbf2d85121b2b99707ff7c2c
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2017-12-16 10:51:15 +0200

    Add netmapreplay

--- netmap/module.mk
+++ netmap/module.mk
@@ -1,5 +1,5 @@
 NETMAP_SRC_LIB :=
-NETMAP_SRC := $(NETMAP_SRC_LIB) netmapfwd.c netmaprecv.c
+NETMAP_SRC := $(NETMAP_SRC_LIB) netmapfwd.c netmaprecv.c netmapreplay.c
 
 NETMAP_SRC_LIB := $(patsubst %,$(DIRNETMAP)/%,$(NETMAP_SRC_LIB))
 NETMAP_SRC := $(patsubst %,$(DIRNETMAP)/%,$(NETMAP_SRC))
@@ -10,11 +10,11 @@ NETMAP_OBJ := $(patsubst %.c,%.o,$(NETMAP_SRC))
 NETMAP_DEP_LIB := $(patsubst %.c,%.d,$(NETMAP_SRC_LIB))
 NETMAP_DEP := $(patsubst %.c,%.d,$(NETMAP_SRC))
 
-CFLAGS_NETMAP := -I$(DIRPACKET) -I$(DIRLINKEDLIST) -I$(DIRIPHDR) -I$(DIRMISC) -I$(DIRLOG) -I$(DIRHASHTABLE) -I$(DIRHASHLIST) -I$(DIRARP) -I$(DIRPORTS) -I$(DIRALLOC)
+CFLAGS_NETMAP := -I$(DIRPACKET) -I$(DIRLINKEDLIST) -I$(DIRIPHDR) -I$(DIRMISC) -I$(DIRLOG) -I$(DIRHASHTABLE) -I$(DIRHASHLIST) -I$(DIRARP) -I$(DIRPORTS) -I$(DIRALLOC) -I$(DIRDYNARR) -I$(DIRMYPCAP)
 
 MAKEFILES_NETMAP := $(DIRNETMAP)/module.mk
 
-LIBS_NETMAP := $(DIRALLOC)/liballoc.a $(DIRIPHDR)/libiphdr.a $(DIRLOG)/liblog.a $(DIRARP)/libarp.a $(DIRPORTS)/libports.a $(DIRHASHTABLE)/libhashtable.a $(DIRHASHLIST)/libhashlist.a
+LIBS_NETMAP := $(DIRALLOC)/liballoc.a $(DIRIPHDR)/libiphdr.a $(DIRLOG)/liblog.a $(DIRARP)/libarp.a $(DIRPORTS)/libports.a $(DIRHASHTABLE)/libhashtable.a $(DIRHASHLIST)/libhashlist.a $(DIRMYPCAP)/libmypcap.a $(DIRDYNARR)/libdynarr.a
 
 .PHONY: NETMAP clean_NETMAP distclean_NETMAP unit_NETMAP $(LCNETMAP) clean_$(LCNETMAP) distclean_$(LCNETMAP) unit_$(LCNETMAP)
 
@@ -26,7 +26,7 @@ unit_$(LCNETMAP): unit_NETMAP
 NETMAP: $(DIRNETMAP)/libnetmap.a
 
 ifeq ($(WITH_NETMAP),yes)
-NETMAP: $(DIRNETMAP)/netmapfwd $(DIRNETMAP)/netmaprecv
+NETMAP: $(DIRNETMAP)/netmapfwd $(DIRNETMAP)/netmaprecv $(DIRNETMAP)/netmapreplay
 CFLAGS_NETMAP += -I$(NETMAP_INCDIR)
 endif
 
@@ -41,6 +41,9 @@ $(DIRNETMAP)/netmapfwd: $(DIRNETMAP)/netmapfwd.o $(DIRNETMAP)/libnetmap.a $(LIBS
 	$(CC) $(CFLAGS) -o $@ $(filter %.o,$^) $(filter %.a,$^) $(CFLAGS_NETMAP) -lpthread
 
 $(DIRNETMAP)/netmaprecv: $(DIRNETMAP)/netmaprecv.o $(DIRNETMAP)/libnetmap.a $(LIBS_NETMAP) $(MAKEFILES_COMMON) $(MAKEFILES_NETMAP)
+	$(CC) $(CFLAGS) -o $@ $(filter %.o,$^) $(filter %.a,$^) $(CFLAGS_NETMAP) -lpthread
+
+$(DIRNETMAP)/netmapreplay: $(DIRNETMAP)/netmapreplay.o $(DIRNETMAP)/libnetmap.a $(LIBS_NETMAP) $(MAKEFILES_COMMON) $(MAKEFILES_NETMAP)
 	$(CC) $(CFLAGS) -o $@ $(filter %.o,$^) $(filter %.a,$^) $(CFLAGS_NETMAP) -lpthread
 
 $(NETMAP_OBJ): %.o: %.c %.d $(MAKEFILES_COMMON) $(MAKEFILES_NETMAP)
---
+++ netmap/netmapreplay.c
@@ -0,0 +1,157 @@
+#define _GNU_SOURCE
+#define NETMAP_WITH_LIBS
+#include <pthread.h>
+#include "dynarr.h"
+#include "arp.h"
+#include "iphdr.h"
+#include "ipcksum.h"
+#include "packet.h"
+#include "net/netmap_user.h"
+#include "mypcap.h"
+#include <sys/poll.h>
+
+static inline uint64_t gettime64(void)
+{
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec*1000UL*1000UL + tv.tv_usec;
+}
+
+struct nm_desc *dlnmd, *ulnmd;
+
+static inline void nm_my_inject(struct nm_desc *nmd, void *data, size_t sz)
+{
+  int i, j;
+  for (i = 0; i < 3; i++)
+  {
+    for (j = 0; j < 3; j++)
+    {
+      if (nm_inject(nmd, data, sz) == 0)
+      {
+        struct pollfd pollfd;
+        pollfd.fd = nmd->fd;
+        pollfd.events = POLLOUT;
+        poll(&pollfd, 1, 0);
+      }
+      else
+      {
+        return;
+      }
+    }
+    ioctl(nmd->fd, NIOCTXSYNC, NULL);
+  }
+}
+
+struct pkt {
+  int len;
+  char data[1514];
+};
+
+int main(int argc, char **argv)
+{
+  struct nmreq nmr;
+  void *buf = NULL;
+  size_t bufcapacity = 0;
+  size_t len = 0;
+  size_t snap = 0;
+  int sz;
+  DYNARR(struct pkt*) ar = DYNARR_INITER;
+  struct pcap_in_ctx inctx;
+  uint64_t bytes = 0, pkts = 0;
+  uint64_t last_bytes = 0, last_pkts = 0;
+  uint64_t last_time64 = 0;
+
+  setlinebuf(stdout);
+
+  if (argc != 3)
+  {
+    printf("usage: %s vale0:1 file.pcap\n", argv[0]);
+    exit(1);
+  }
+
+  memset(&nmr, 0, sizeof(nmr));
+  nmr.nr_rx_slots = 256;
+  nmr.nr_tx_slots = 64;
+  dlnmd = nm_open(argv[1], &nmr, 0, NULL);
+  if (dlnmd == NULL)
+  {
+    printf("cannot open %s\n", argv[1]);
+    exit(1);
+  }
+
+  if (pcap_in_ctx_init(&inctx, argv[2], 1) != 0)
+  {
+    printf("cannot open %s\n", argv[2]);
+    exit(1);
+  }
+
+  for (;;)
+  {
+    int ret;
+    struct pollfd pfds[2];
+    struct pkt *pkt;
+    uint64_t time64;
+    pfds[0].fd = dlnmd->fd;
+    pfds[0].events = POLLIN;
+    pfds[1].fd = ulnmd->fd;
+    pfds[1].events = POLLIN;
+    poll(pfds, 2, 5);
+    time64 = gettime64();
+    ret = pcap_in_ctx_read(&inctx, &buf, &bufcapacity, &len, &snap, &time64);
+    if (ret == 0)
+    {
+      break;
+    }
+    else if (ret != 1)
+    {
+      printf("can't read pcap\n");
+      exit(1);
+    }
+    if (len != snap)
+    {
+      printf("truncated pcap\n");
+      exit(1);
+    }
+    if (len > sizeof(pkt->data))
+    {
+      printf("overlong packet\n");
+      exit(1);
+    }
+    pkt = malloc(sizeof(*pkt));
+    if (pkt == NULL)
+    {
+      printf("out of memory\n");
+      exit(1);
+    }
+    memcpy(pkt->data, buf, len);
+    pkt->len = len;
+    if (!DYNARR_PUSH_BACK(&ar, pkt))
+    {
+      printf("out of memory\n");
+      exit(1);
+    }
+  }
+  sz = DYNARR_SIZE(&ar);
+  last_time64 = gettime64();
+  for (;;)
+  {
+    for (int i = 0; i < sz; i++)
+    {
+      nm_my_inject(dlnmd, DYNARR_GET(&ar, i)->data, DYNARR_GET(&ar, i)->len);
+      pkts++;
+      bytes += DYNARR_GET(&ar, i)->len;
+      if (pkts % 16 == 0 && gettime64() >= last_time64 + 1000*1000)
+      {
+        uint64_t time64 = gettime64();
+        uint64_t pdiff = pkts - last_pkts;
+        uint64_t bdiff = bytes - last_bytes;
+        double tdiff = time64 - last_time64;
+        printf("%g MPPS %g Gbps\n", pdiff/tdiff, bdiff*8/1000/tdiff);
+        last_pkts = pkts;
+        last_bytes = bytes;
+        last_time64 = time64;
+      }
+    }
+  }
+  return 0;
+}
