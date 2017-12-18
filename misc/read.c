dd2e4f167a7b38d433209f2fafb6c007d84ac7c8
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2017-12-18 18:26:15 +0200

    Controlled exit for netmapproxy1

--- misc/read.c
+++ misc/read.c
@@ -1,5 +1,72 @@
 #include "read.h"
 #include <errno.h>
+#include <sys/poll.h>
+#include <stdio.h>
+
+int accept_interrupt(int fd, struct sockaddr *addr, socklen_t *addrlen, int interruptfd)
+{
+  char ch;
+  struct pollfd pfds[2];
+  int ret;
+  for (;;)
+  {
+    pfds[0].fd = fd;
+    pfds[1].fd = interruptfd;
+    pfds[0].events = POLLIN;
+    pfds[1].events = POLLIN;
+    poll(pfds, 2, -1);
+    if (read(interruptfd, &ch, 1) > 0)
+    {
+      errno = EINTR;
+      return -1;
+    }
+    ret = accept(fd, addr, addrlen);
+    if (ret >= 0)
+    {
+      return ret;
+    }
+  }
+}
+
+ssize_t readall_interrupt(int fd, void *buf, size_t count, int interruptfd)
+{
+  size_t bytes_read = 0;
+  char *cbuf = buf;
+  ssize_t ret;
+  struct pollfd pfds[2];
+  char ch;
+  while (bytes_read < count)
+  {
+    pfds[0].fd = fd;
+    pfds[1].fd = interruptfd;
+    pfds[0].events = POLLIN;
+    pfds[1].events = POLLIN;
+    poll(pfds, 2, -1);
+    if (read(interruptfd, &ch, 1) > 0)
+    {
+      errno = EINTR;
+      return -1;
+    }
+    ret = read(fd, cbuf + bytes_read, count - bytes_read);
+    if (ret > 0)
+    {
+      bytes_read += ret;
+    }
+    if (ret == 0)
+    {
+      break;
+    }
+    if (ret < 0 && errno == EINTR)
+    {
+      continue;
+    }
+    if (ret < 0)
+    {
+      return ret;
+    }
+  }
+  return bytes_read;
+}
 
 ssize_t readall(int fd, void *buf, size_t count)
 {
--- misc/read.h
+++ misc/read.h
@@ -1,9 +1,15 @@
 #ifndef _READ_H_
 #define _READ_H_
 
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <unistd.h>
 #include <stddef.h>
 
+int accept_interrupt(int fd, struct sockaddr *addr, socklen_t *addrlen, int interruptfd);
+
 ssize_t readall(int fd, void *buf, size_t count);
+
+ssize_t readall_interrupt(int fd, void *buf, size_t count, int interruptfd);
 
 #endif
