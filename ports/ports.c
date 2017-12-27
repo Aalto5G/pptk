301d668e42b3a717ca846d3dc7095e223e492bae
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2017-12-28 00:22:42 +0200

    Add generic allocator discard function to ports

--- ports/ports.c
+++ ports/ports.c
@@ -1,5 +1,11 @@
 #include "ports.h"
 
+void allocifdiscardfunc(struct packet *pkt, void *userdata)
+{
+  struct allocifdiscardfunc_userdata *ud = userdata;
+  allocif_free(ud->intf, pkt);
+}
+
 void linkedlistfunc(struct packet *pkt, void *userdata)
 {
   struct linkedlistfunc_userdata *ud = userdata;
--- ports/ports.h
+++ ports/ports.h
@@ -2,12 +2,19 @@
 #define _PORTS_H_
 
 #include "packet.h"
+#include "allocif.h"
 #include "linkedlist.h"
 
 struct port {
   void (*portfunc)(struct packet *pkt, void *userdata);
   void *userdata;
 };
+
+struct allocifdiscardfunc_userdata {
+  struct allocif *intf;
+};
+
+void allocifdiscardfunc(struct packet *pkt, void *userdata);
 
 struct linkedlistfunc_userdata {
   struct linked_list_head *head;
