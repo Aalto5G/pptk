3f01b64c589f9486602104617b8cf8461141c958
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2017-12-28 21:25:54 +0200

    Fix Linux implementation

--- ipfrag/linux.c
+++ ipfrag/linux.c
@@ -70,11 +70,11 @@ struct packet *ip_frag_reassemble(struct allocif *loc, struct ipq *qp)
   pkt = qp->q.fragments;
   //printf("%d\n", qp->q.len);
   //printf("%d\n", qp->q.meat);
-  pkt2 = allocif_alloc(loc, packet_size(qp->q.len));
-  ether2 = packet_data(pkt2);
-  ip2 = ether_payload(ether2);
   ether = packet_data(pkt);
   ip = ether_payload(ether);
+  pkt2 = allocif_alloc(loc, packet_size(14 + ip_hdr_len(ip) + qp->q.len));
+  ether2 = packet_data(pkt2);
+  ip2 = ether_payload(ether2);
   //printf("copying %d\n", 14 + ip_hdr_len(ip));
   pkt2->sz = qp->q.len + 14 + ip_hdr_len(ip);
   memcpy(ether2, ether, 14 + ip_hdr_len(ip));
@@ -105,10 +105,25 @@ int ip_frag_queue(struct allocif *loc, struct ipq *qp, struct packet *pkt)
         int ihl, end;
         int err = -ENOENT;
 
+#if 0
+	printf("-------- QUEUE ---------\n");
+	for (next = qp->q.fragments; next != NULL;
+             next = CONTAINER_OF(next->node.next, struct packet, node))
+	{
+		printf("%d-%d\n", (int)next->positive.offset,
+		       (int)next->positive.offset +
+		       (int)(next->sz - 14 - 20 - next->positive.pulled));
+	}
+	printf("-------- END QUEUE ---------\n");
+#endif
+
         pkt->positive.pulled = 0;
 
         if (qp->q.flags & INET_FRAG_COMPLETE)
+	{
+		//printf("err0\n");
                 goto err;
+	}
 
 #if 0
         if (!(pkt->positive.flags & IPSKB_FRAG_COMPLETE) &&
@@ -132,22 +147,34 @@ int ip_frag_queue(struct allocif *loc, struct ipq *qp, struct packet *pkt)
                  */
                 if (end < qp->q.len ||
                     ((qp->q.flags & INET_FRAG_LAST_IN) && end != qp->q.len))
+		{
+			//printf("err1\n");
                         goto err;
+		}
                 qp->q.flags |= INET_FRAG_LAST_IN;
                 qp->q.len = end;
         } else {
                 if (end&7) {
-                        end &= ~7;
+                        //end &= ~7; // FIXME add back
                 }
                 if (end > qp->q.len) {
                         /* Some bits beyond end -> corruption. */
                         if (qp->q.flags & INET_FRAG_LAST_IN)
+			{
+				//printf("err2\n");
                                 goto err;
+			}
                         qp->q.len = end;
                 }
         }
         if (end == offset)
-                goto err;
+	{
+        	allocif_free(loc, pkt);
+        	return -EINPROGRESS;
+	}
+#if 0
+	printf("ADD %d-%d\n", offset, (int)(offset + pkt->sz - 14 - ihl));
+#endif
 
         err = -ENOMEM;
         //pkt->positive.pulled += 14 + ihl;
@@ -181,7 +208,12 @@ found:
                         offset += i;
                         err = -EINVAL;
                         if (end <= offset)
-                                goto err;
+			{
+        			allocif_free(loc, pkt);
+				return -EINPROGRESS;
+				//printf("err4\n");
+                                //goto err;
+			}
                         err = -ENOMEM;
                         pkt->positive.pulled += i;
                 }
@@ -251,5 +283,6 @@ found:
 
 err:
         allocif_free(loc, pkt);
+	//printf("ERROR\n");
         return err;
 }
