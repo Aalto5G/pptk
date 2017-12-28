a4f044f990eabb5f7148c2e831b6b29ac1eb3c57
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2017-12-28 02:36:51 +0200

    Direct vs indirect allocation performance

--- alloc/allociftest.c
+++ alloc/allociftest.c
@@ -1,16 +1,34 @@
 #include "allocif.h"
 #include "llalloc.h"
+#include "time64.h"
 
 int main(int argc, char **argv)
 {
   struct ll_alloc_st st = {};
   struct allocif intf = {.ops = &ll_allocif_ops_st, .userdata = &st};
   void *block;
+  int i;
+  uint64_t begin, end;
 
   ll_alloc_st_init(&st, 1000, 4096);
 
-  block = allocif_alloc(&intf, 1024);
-  allocif_free(&intf, block);
+  begin = gettime64();
+  for (i = 0; i < 100*1000*1000; i++)
+  {
+    block = allocif_alloc(&intf, 1024);
+    allocif_free(&intf, block);
+  }
+  end = gettime64();
+  printf("%g MPPS indirect\n", 1e8/(end-begin));
+
+  begin = gettime64();
+  for (i = 0; i < 100*1000*1000; i++)
+  {
+    block = ll_alloc_st(&st, 1024);
+    ll_free_st(&st, block);
+  }
+  end = gettime64();
+  printf("%g MPPS direct\n", 1e8/(end-begin));
 
   ll_alloc_st_free(&st);
 
