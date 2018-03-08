5a549f14bb1ee0923568ca5b3236bbf0e0829a26
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2018-03-08 22:02:20 +0200

    Burst writes for ODP ports

--- ports/odpports.c
+++ ports/odpports.c
@@ -101,3 +101,127 @@ void odpfunc2(struct packet *pkt, void *userdata)
   }
   allocif_free(ud->intf, pkt);
 }
+
+void odpfunc3flushdl(struct odpfunc3_userdata *ud)
+{
+  int sent;
+  int lost;
+  sent = odp_pktout_send(ud->dloutq, ud->dloutbuf, ud->dloutcnt);
+  if (sent < 0)
+  {
+    sent = 0;
+  }
+  lost = ud->dloutcnt - sent;
+  ud->dloutcnt = 0;
+  odp_packet_free_multi(&ud->dloutbuf[sent], lost);
+}
+
+void odpfunc3flushul(struct odpfunc3_userdata *ud)
+{
+  int sent;
+  int lost;
+  sent = odp_pktout_send(ud->uloutq, ud->uloutbuf, ud->uloutcnt);
+  if (sent < 0)
+  {
+    sent = 0;
+  }
+  lost = ud->uloutcnt - sent;
+  ud->uloutcnt = 0;
+  odp_packet_free_multi(&ud->uloutbuf[sent], lost);
+}
+
+void odpfunc3(struct packet *pkt, void *userdata)
+{
+  struct odpfunc3_userdata *ud = userdata;
+  odp_packet_t odppkt;
+  char *buf;
+  if (pkt->direction == PACKET_DIRECTION_UPLINK)
+  {
+    odppkt = odp_packet_alloc(ud->pool, pkt->sz);
+    buf = odp_packet_data(odppkt);
+    memcpy(buf, packet_data(pkt), pkt->sz);
+    if (ud->uloutcnt >= sizeof(ud->uloutbuf)/sizeof(*ud->uloutbuf))
+    {
+      odpfunc3flushul(ud);
+    }
+    if (ud->uloutcnt >= sizeof(ud->uloutbuf)/sizeof(*ud->uloutbuf))
+    {
+      abort();
+    }
+    ud->uloutbuf[ud->uloutcnt++] = odppkt;
+#if 0
+    sent = odp_pktout_send(ud->uloutq, &odppkt, 1);
+    if (sent < 0)
+    {
+      sent = 0;
+    }
+    if (sent == 0)
+    {
+      odp_packet_free(odppkt);
+    }
+#endif
+    if (ud->wan)
+    {
+      if (pcapng_out_ctx_write(ud->wanctx, packet_data(pkt), pkt->sz,
+          gettime64(), "out"))
+      {
+        log_log(LOG_LEVEL_CRIT, "PORTS", "can't record packet");
+        exit(1);
+      }
+    }
+    if (ud->out)
+    {
+      if (pcapng_out_ctx_write(ud->outctx, packet_data(pkt), pkt->sz,
+          gettime64(), "out"))
+      {
+        log_log(LOG_LEVEL_CRIT, "PORTS", "can't record packet");
+        exit(1);
+      }
+    }
+  }
+  else
+  {
+    odppkt = odp_packet_alloc(ud->pool, pkt->sz);
+    buf = odp_packet_data(odppkt);
+    memcpy(buf, packet_data(pkt), pkt->sz);
+    if (ud->dloutcnt >= sizeof(ud->dloutbuf)/sizeof(*ud->dloutbuf))
+    {
+      odpfunc3flushdl(ud);
+    }
+    if (ud->dloutcnt >= sizeof(ud->dloutbuf)/sizeof(*ud->dloutbuf))
+    {
+      abort();
+    }
+    ud->dloutbuf[ud->dloutcnt++] = odppkt;
+#if 0
+    sent = odp_pktout_send(ud->dloutq, &odppkt, 1);
+    if (sent < 0)
+    {
+      sent = 0;
+    }
+    if (sent == 0)
+    {
+      odp_packet_free(odppkt);
+    }
+#endif
+    if (ud->lan)
+    {
+      if (pcapng_out_ctx_write(ud->lanctx, packet_data(pkt), pkt->sz,
+          gettime64(), "out"))
+      {
+        log_log(LOG_LEVEL_CRIT, "PORTS", "can't record packet");
+        exit(1);
+      }
+    }
+    if (ud->out)
+    {
+      if (pcapng_out_ctx_write(ud->outctx, packet_data(pkt), pkt->sz,
+          gettime64(), "in"))
+      {
+        log_log(LOG_LEVEL_CRIT, "PORTS", "can't record packet");
+        exit(1);
+      }
+    }
+  }
+  allocif_free(ud->intf, pkt);
+}
--- ports/odpports.h
+++ ports/odpports.h
@@ -10,6 +10,8 @@ void odpfunc(struct packet *pkt, void *userdata);
 
 void odpfunc2(struct packet *pkt, void *userdata);
 
+void odpfunc3(struct packet *pkt, void *userdata);
+
 struct odpfunc_userdata {
   struct allocif *intf;
   odp_pool_t pool;
@@ -28,5 +30,25 @@ struct odpfunc2_userdata {
   struct pcapng_out_ctx *wanctx;
   struct pcapng_out_ctx *outctx;
 };
+
+struct odpfunc3_userdata {
+  struct allocif *intf;
+  odp_pool_t pool;
+  odp_pktout_queue_t uloutq;
+  odp_pktout_queue_t dloutq;
+  odp_packet_t uloutbuf[1000];
+  odp_packet_t dloutbuf[1000];
+  size_t uloutcnt;
+  size_t dloutcnt;
+  int lan;
+  int wan;
+  int out;
+  struct pcapng_out_ctx *lanctx;
+  struct pcapng_out_ctx *wanctx;
+  struct pcapng_out_ctx *outctx;
+};
+
+void odpfunc3flushdl(struct odpfunc3_userdata *ud);
+void odpfunc3flushul(struct odpfunc3_userdata *ud);
 
 #endif
