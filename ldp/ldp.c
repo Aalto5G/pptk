7a78f8ded2bac54c644f87f462ed0240c7411027
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2018-04-06 17:01:48 +0300

    Socket version of LDP

--- ldp/ldp.c
+++ ldp/ldp.c
@@ -1,9 +1,14 @@
 #define NETMAP_WITH_LIBS
+#define _GNU_SOURCE
 
 #include <sys/poll.h>
+#include <sys/socket.h>
 #include <linux/ethtool.h>
+#include <linux/if_packet.h>
 #include <linux/sockios.h>
+#include <linux/if_ether.h>
 #include <net/if.h>
+#include <arpa/inet.h>
 #include "net/netmap_user.h"
 #include "ldp.h"
 
@@ -19,6 +24,44 @@ struct ldp_out_queue_netmap {
   struct ldp_out_queue q;
   struct nm_desc *nmd;
 };
+
+struct ldp_in_queue_socket {
+  struct ldp_in_queue q;
+  int max_sz;
+  int num_bufs;
+  char **bufs;
+};
+
+struct ldp_out_queue_socket {
+  struct ldp_out_queue q;
+};
+
+static void ldp_in_queue_close_socket(struct ldp_in_queue *inq)
+{
+  int i;
+  struct ldp_in_queue_socket *insock;
+  insock = CONTAINER_OF(inq, struct ldp_in_queue_socket, q);
+  close(insock->q.fd);
+  for (i = 0; i < insock->num_bufs; i++)
+  {
+    free(insock->bufs[i]);
+  }
+  free(insock->bufs);
+  free(insock);
+}
+
+static void ldp_out_queue_close_socket(struct ldp_out_queue *outq)
+{
+  struct ldp_out_queue_socket *outsock;
+  outsock = CONTAINER_OF(outq, struct ldp_out_queue_socket, q);
+  close(outsock->q.fd);
+  free(outsock);
+}
+
+static int ldp_out_queue_txsync_socket(struct ldp_out_queue *outq)
+{
+  return 0;
+}
 
 static void ldp_in_queue_close_netmap(struct ldp_in_queue *inq)
 {
@@ -76,6 +119,72 @@ static int ldp_in_queue_poll(struct ldp_in_queue *inq, uint64_t timeout_usec)
   timeout.tv_sec = timeout_usec / (1000*1000);
   timeout.tv_usec = timeout_usec % (1000*1000);
   return select(nfds, &set, NULL, NULL, &timeout);
+}
+
+static int ldp_in_queue_nextpkts_socket(struct ldp_in_queue *inq,
+                                        struct ldp_packet *pkts, int num)
+{
+  int i;
+  int fd = inq->fd;
+  int ret;
+  struct ldp_in_queue_socket *insock;
+  insock = CONTAINER_OF(inq, struct ldp_in_queue_socket, q);
+
+  if (num > insock->num_bufs)
+  {
+    num = insock->num_bufs;
+  }
+
+  struct mmsghdr msgs[num];
+  struct iovec iovecs[num][1];
+
+  memset(msgs, 0, sizeof(msgs));
+  memset(iovecs, 0, sizeof(iovecs));
+
+  for (i = 0; i < num; i++)
+  {
+    msgs[i].msg_hdr.msg_iovlen = 1;
+    msgs[i].msg_hdr.msg_iov = iovecs[i];
+    iovecs[i][0].iov_base = insock->bufs[i];
+    iovecs[i][0].iov_len = insock->max_sz;
+  }
+  ret = recvmmsg(fd, msgs, num, MSG_DONTWAIT, NULL);
+  for (i = 0; i < ret; i++)
+  {
+    // FIXME bypass own packets
+    pkts[i].data = insock->bufs[i];
+    pkts[i].sz = msgs[i].msg_len;
+  }
+  return ret;
+}
+
+static int ldp_out_queue_inject_socket(struct ldp_out_queue *outq,
+                                       struct ldp_packet *pkts, int num)
+{
+  int i;
+  int fd = outq->fd;
+  int ret;
+
+  if (num <= 0)
+  {
+    return 0;
+  }
+
+  struct mmsghdr msgs[num];
+  struct iovec iovecs[num][1];
+
+  memset(msgs, 0, sizeof(msgs));
+  memset(iovecs, 0, sizeof(iovecs));
+
+  for (i = 0; i < num; i++)
+  {
+    msgs[i].msg_hdr.msg_iovlen = 1;
+    msgs[i].msg_hdr.msg_iov = iovecs[i];
+    iovecs[i][0].iov_base = pkts[i].data;
+    iovecs[i][0].iov_len = pkts[i].sz;
+  }
+  ret = sendmmsg(fd, msgs, num, 0);
+  return ret;
 }
 
 static int ldp_in_queue_nextpkts_netmap(struct ldp_in_queue *inq,
@@ -162,6 +271,142 @@ static int check_channels(const char *name, int numinq)
   return 1;
 }
 
+static struct ldp_interface *
+ldp_interface_open_socket(const char *name, int numinq, int numoutq)
+{
+  struct ldp_interface *intf;
+  struct ldp_in_queue **inqs;
+  struct ldp_out_queue **outqs;
+  struct ldp_in_queue_socket *insock;
+  struct ldp_out_queue_socket *outsock;
+  struct sockaddr_ll sockaddr_ll;
+  int i;
+
+  if (numinq != 1 || numoutq != 1)
+  {
+    return NULL;
+  }
+  intf = malloc(sizeof(*intf));
+  if (intf == NULL)
+  {
+    abort(); // FIXME better error handling
+  }
+  inqs = malloc(numinq*sizeof(*inqs));
+  if (inqs == NULL)
+  {
+    abort(); // FIXME better error handling
+  }
+  outqs = malloc(numoutq*sizeof(*outqs));
+  if (outqs == NULL)
+  {
+    abort(); // FIXME better error handling
+  }
+
+
+  insock = malloc(sizeof(*insock));
+  if (insock == NULL)
+  {
+    abort();
+  }
+  insock->q.fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+  if (insock->q.fd < 0)
+  {
+    abort();
+  }
+
+  struct ifreq ifr;
+  int ifindex;
+  memset(&ifr, 0, sizeof(ifr));
+  snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), "%s", name);
+  if (ioctl(insock->q.fd, SIOCGIFINDEX, &ifr) != 0)
+  {
+    close(insock->q.fd);
+    free(insock);
+    free(inqs);
+    free(outqs);
+    free(intf);
+    return NULL;
+  }
+  ifindex = ifr.ifr_ifindex;
+  memset(&sockaddr_ll, 0, sizeof(sockaddr_ll));
+  sockaddr_ll.sll_family = AF_PACKET;
+  sockaddr_ll.sll_ifindex = ifindex;
+  sockaddr_ll.sll_protocol = htons(ETH_P_ALL);
+  if (bind(insock->q.fd,
+           (struct sockaddr*)&sockaddr_ll, sizeof(sockaddr_ll)) < 0)
+  {
+    close(insock->q.fd);
+    free(insock);
+    free(inqs);
+    free(outqs);
+    free(intf);
+    return NULL;
+  }
+
+  outsock = malloc(sizeof(*outsock));
+  if (outsock == NULL)
+  {
+    abort();
+  }
+  outsock->q.fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+  if (outsock->q.fd < 0)
+  {
+    abort();
+  }
+  int one = 1;
+  setsockopt(outsock->q.fd, SOL_PACKET, PACKET_QDISC_BYPASS, &one, sizeof(one));
+
+  memset(&sockaddr_ll, 0, sizeof(sockaddr_ll));
+  sockaddr_ll.sll_family = AF_PACKET;
+  sockaddr_ll.sll_ifindex = ifindex;
+  sockaddr_ll.sll_protocol = htons(ETH_P_ALL);
+  if (bind(outsock->q.fd,
+           (struct sockaddr*)&sockaddr_ll, sizeof(sockaddr_ll)) < 0)
+  {
+    close(outsock->q.fd);
+    free(outsock);
+    close(insock->q.fd);
+    free(insock);
+    free(inqs);
+    free(outqs);
+    free(intf);
+    return NULL;
+  }
+
+  insock->q.nextpkts = ldp_in_queue_nextpkts_socket;
+  insock->q.poll = ldp_in_queue_poll;
+  insock->q.close = ldp_in_queue_close_socket;
+
+  outsock->q.inject = ldp_out_queue_inject_socket;
+  outsock->q.txsync = ldp_out_queue_txsync_socket;
+  outsock->q.close = ldp_out_queue_close_socket;
+
+  inqs[0] = &insock->q;
+  outqs[0] = &outsock->q;
+  
+  intf->num_inq = numinq;
+  intf->num_outq = numoutq;
+  intf->inq = inqs;
+  intf->outq = outqs;
+
+  insock->max_sz = 1514;
+  insock->num_bufs = 128;
+  insock->bufs = malloc(insock->num_bufs*sizeof(*insock->bufs));
+  if (insock->bufs == NULL)
+  {
+    abort();
+  }
+  for (i = 0; i < insock->num_bufs; i++)
+  {
+    insock->bufs[i] = malloc(insock->max_sz);
+    if (insock->bufs[i] == NULL)
+    {
+      abort();
+    }
+  }
+
+  return intf;
+}
 
 struct ldp_interface *
 ldp_interface_open(const char *name, int numinq, int numoutq)
@@ -192,7 +437,7 @@ ldp_interface_open(const char *name, int numinq, int numoutq)
   }
   else
   {
-    return NULL;
+    return ldp_interface_open_socket(name, numinq, numoutq);
   }
   max = numinq;
   if (max < numoutq)
