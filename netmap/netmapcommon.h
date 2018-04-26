0243fcdac35220f38bf19f94c290e92ed32d659c
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2018-04-26 17:07:18 +0300

    Simplify nm_my_inject function

--- netmap/netmapcommon.h
+++ netmap/netmapcommon.h
@@ -2,28 +2,19 @@
 #define _NETMAPCOMMON_H_
 
 #include <sys/poll.h>
+#include "branchpredict.h"
 
 static inline void nm_my_inject(struct nm_desc *nmd, void *data, size_t sz)
 {
-  int i, j;
-  for (i = 0; i < 3; i++)
+  if (likely(nm_inject(nmd, data, sz) != 0))
   {
-    for (j = 0; j < 3; j++)
-    {
-      if (nm_inject(nmd, data, sz) == 0)
-      {
-        struct pollfd pollfd;
-        pollfd.fd = nmd->fd;
-        pollfd.events = POLLOUT;
-        poll(&pollfd, 1, 0);
-      }
-      else
-      {
-        return;
-      }
-    }
-    ioctl(nmd->fd, NIOCTXSYNC, NULL);
+    return;
   }
+  struct pollfd pollfd;
+  pollfd.fd = nmd->fd;
+  pollfd.events = POLLOUT;
+  poll(&pollfd, 1, -1);
+  nm_inject(nmd, data, sz);
 }
 
 void set_promisc_mode(int sockfd, const char *ifname, int on);
