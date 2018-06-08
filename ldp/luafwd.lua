9ad98a44f50ca3f47a3159952192ddd1d185939a
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2018-06-08 16:18:28 +0300

    One-based indexing and bounds check for Lua LDP

--- ldp/luafwd.lua
+++ ldp/luafwd.lua
@@ -1,10 +1,16 @@
 Ldp = require 'lualdp'
 
 intf1 = Ldp.interface_open("vale0:1", 1, 1)
+if not intf1 then
+  error("Can't open intf1")
+end
 intf2 = Ldp.interface_open("vale1:1", 1, 1)
+if not intf2 then
+  error("Can't open intf2")
+end
 
-inq = Ldp.get_inq(intf1, 0)
-outq = Ldp.get_outq(intf2, 0)
+inq = Ldp.get_inq(intf1, 1)
+outq = Ldp.get_outq(intf2, 1)
 
 while not Ldp.inq_eof(inq) do
   Ldp.inq_poll(inq, 10000)
