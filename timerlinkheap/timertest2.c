78b3f128b80c8037cd6e10bda4e31ed949bb85b3
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2018-07-30 13:09:40 +0300

    Make timer load more heavy in timerlinkheap/timertest2.c

--- timerlinkheap/timertest2.c
+++ timerlinkheap/timertest2.c
@@ -4,12 +4,12 @@
 #include <unistd.h>
 #include "time64.h"
 
-#define MIN_PERIOD (10*1000)
+#define MIN_PERIOD_NSEC (1000)
 #define PERIOD_MUL 5
 #define PERIOD_DIV 4
 
 struct periodud {
-  uint64_t period;
+  uint64_t periodnsec;
 };
 
 static void periodic_fn(
@@ -24,7 +24,7 @@ static void periodic_fn(
   }
   printf("periodic timer, period %g\n", ud->period/1000.0/1000.0);
 #endif
-  timer->time64 = gettime64() + ud->period;
+  timer->time64 = gettime64() + ud->periodnsec/1000;
   timer_linkheap_add(heap, timer);
 #if 0
   if (!timer_linkheap_verify(heap))
@@ -41,21 +41,21 @@ int main(int argc, char **argv)
   struct timer_link periodics[50];
   size_t i;
   timer_linkheap_init(&heap);
-  periodics_ud[0].period = MIN_PERIOD;
-  periodics[0].time64 = gettime64() + MIN_PERIOD;
+  periodics_ud[0].periodnsec = MIN_PERIOD_NSEC;
+  periodics[0].time64 = gettime64() + periodics_ud[0].periodnsec/1000;
   periodics[0].fn = periodic_fn;
   periodics[0].userdata = &periodics_ud[0];
   timer_linkheap_add(&heap, &periodics[0]);
   for (i = 1; i < 50; i++)
   {
-    periodics_ud[i].period = periodics_ud[i-1].period*PERIOD_MUL/PERIOD_DIV;
-    periodics[i].time64 = gettime64() + periodics_ud[i].period;
+    periodics_ud[i].periodnsec = periodics_ud[i-1].periodnsec*PERIOD_MUL/PERIOD_DIV;
+    periodics[i].time64 = gettime64() + periodics_ud[i].periodnsec/1000;
     periodics[i].fn = periodic_fn;
     periodics[i].userdata = &periodics_ud[i];
     timer_linkheap_add(&heap, &periodics[i]);
   }
 
-  alarm(60);
+  alarm(6);
 
   for (;;)
   {
@@ -71,7 +71,7 @@ int main(int argc, char **argv)
     timer->fn(timer, &heap, timer->userdata, NULL);
 
     i = rand()%50;
-    periodics[i].time64 += periodics_ud[i].period;
+    periodics[i].time64 += periodics_ud[i].periodnsec/1000;
     timer_linkheap_modify(&heap, &periodics[i]);
 
     i = rand()%50;
